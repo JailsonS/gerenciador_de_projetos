9d95988b138c1de1658bebca5ef6a173e7838b49
Jailson Soares de Souza Filho <jailson.filho@techlead.local>
2021-08-13 21:02:04 -0300

    update

--- app/Models/User/RepositoryUser.php
+++ app/Models/User/RepositoryUser.php
@@ -2,9 +2,10 @@
 namespace App\Models\User;
 
 use App\Models\Email;
+use App\Models\Phone;
 
 interface RepositoryUser
 {
     public function doLogin(Email $email, string $password): void;
-    public function create(string $name, Email $email, Phone $phone): void;
+    public function createUser(string $name, Email $email, Phone $phone): void;
 }
