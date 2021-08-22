bc602f9fdadcecb3b0a8f22a8e9748bc999d11d0
Jailson Soares de Souza Filho <jailson.filho@techlead.local>
2021-08-22 11:29:21 -0300

    update

--- app/Models/User/RepositoryUser.php
+++ app/Models/User/RepositoryUser.php
@@ -3,9 +3,10 @@ namespace App\Models\User;
 
 use App\Models\Email;
 use App\Models\Phone;
+use App\Models\User\EncryptPassword;
 
 interface RepositoryUser
 {
-    public function doLogin(Email $email, string $password): void;
+    public function doLogin(Email $email, string $password, EncryptPassword $encryptPassword): void;
     public function createUser(string $name, Email $email, Phone $phone): void;
 }
